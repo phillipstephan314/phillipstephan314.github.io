b0a5f80ce0884ae113bc76476046fabd8a4451a0
phillipstephan314 <122295953+phillipstephan314@users.noreply.github.com>
2023-01-09 18:15:56 +0100

    Add files via upload

--- script3.js
+++ script3.js
@@ -9,7 +9,7 @@ function draw() {
     ctx.clearRect(0,0,600,600);
 z=10;
 while(z<600){
-ctx.fillStyle = "rgba(255, 0, 0, 1)";
+ctx.fillStyle = "white";
     ctx.fillRect(z, 300, 1, 1);
     ctx.fillRect(300, z, 1, 1);
 z=z+10;
@@ -17,7 +17,7 @@ z=z+10;
 let x=-100;
 while(x<100){
 y=a*Math.pow(x,b)+c*Math.pow(x,d);
-ctx.fillStyle = "#FF10F0";
+ctx.fillStyle = "#37ff00";
     ctx.fillRect(10*x+300, 10*((1-y))+290, 1, 1);
 x=x+0.0001;
   }
