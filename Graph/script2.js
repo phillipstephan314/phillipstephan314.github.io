6ed54deb03ae9dfb1a6d34cd39f97b16d37be9c2
phillipstephan314 <122295953+phillipstephan314@users.noreply.github.com>
2023-01-26 19:28:01 +0100

    Delete Graph directory

--- Graph/script2.js
+++
@@ -1,7 +0,0 @@
-function write(){let a = document.getElementById("userInput").value;
-let b = document.getElementById("userInput1").value;
-let c = document.getElementById("userInput2").value;
-let d = document.getElementById("userInput3").value; 
-
-console.log("y=");
-}
